d17d513c1c360b5009554ead2fc6b1b4465475b4
devendra540 <illuridevendra3@gmail.com>
2026-07-14 12:07:32 +0530

    Update App.java

--- src/main/java/com/devendra/devops/App.java
+++ src/main/java/com/devendra/devops/App.java
@@ -1,7 +1,20 @@
 package com.devendra.devops;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Devendra's AWS DevOps Project!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Devendra's AWS DevOps Project!";
     }
 }
